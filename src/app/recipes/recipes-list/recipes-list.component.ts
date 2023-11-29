6098ac582b300cdd630188f62f4475dfd12ad16b
hongjunpyo11 <jpyo98@naver.com>
2023-11-29 20:33:26 +0900

    ngFor로 레시피 리스트 출력하기

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './recipes-list.component.css'
 })
 export class RecipesListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', "This is simply a test", 'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg'),
+    new Recipe('A Test Recipe', "This is simply a test", 'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg')
+  ];
 
   constructor() {
   }
